fefa0503ac16c881dc05fcb7981eeed486115d5f
CLarmour <connorlarmour@outlook.com>
2015-12-01 22:36:08 +0000

    initial commit, created contact model and migrated

---
+++ db/migrate/20151201210145_contact_model.rb
@@ -0,0 +1,16 @@
+class ContactModel < ActiveRecord::Migration
+  def change
+   # Add code to create the table here
+   # HINT: check out ActiveRecord::Migration.create_table
+   create_table :contact do |t|
+     # column definitions go here
+     # Use the AR migration guide for syntax reference
+     t.string :name
+     t.string :email
+     t.string :address
+     t.string :phone
+     t.timestamps
+    end
+  end
+
+end
